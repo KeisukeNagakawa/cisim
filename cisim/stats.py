02bcf4fc036e52bff7be8d33faceb4a36b683279
Keisuke Nagakawa <28346899+KeisukeNagakawa@users.noreply.github.com>
2020-07-16 10:16:43 +0900

    testcaseごとにインスタンスを変更可能にした

--- cisim/stats.py
+++ cisim/stats.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import binom
+from scipy.stats import binom, hypergeom
 from scipy.optimize import minimize_scalar
 
 
@@ -8,19 +8,19 @@ class BinomCI:
     二項分布に関するクラス
     """
 
-    def __init__(self, n_a, n_x, cl=0.05):
-        self.n_a = n_a  # number of all trials
-        self.n_x = n_x  # number of observed success
+    def __init__(self, n_pop, n_obs, cl=0.05):
+        self.n_pop = n_pop  # number of all trials
+        self.n_obs = n_obs  # number of observed success
         self.cl = cl  # confidence level
 
-    def diff_of_tail_area_and_cl(self, p, n_t, lf='left'):
+    def diff_of_tail_area_and_cl(self, p, lf='left'):
         """
         :param n_t: しきい値となる成功数
         :param lf: レフトテイル（left）かライトテイル（right）か
         :param p: 母比率
         :param cl: 信頼水準
         :return:lf='left'の場合は[0:n_x+1]の区間における確率分布の面積を、
-        lf='right'の場合は[n_x+1:n_a+1]における確率分布の面積を算出し、
+        lf='right'の場合は[n_x:n_pop+1]における確率分布の面積を算出し、
         それらと信頼水準（cl: confidence level）/2の差の絶対値を算出する。
         （最適化関数で最小化するために用いる）
         """
@@ -28,9 +28,9 @@ class BinomCI:
             # 最適化関数に食わせるときに[0,1]を超える場合があるので抑えておく。
             return 100000
         if lf == 'left':  # left tailを計算
-            return abs(binom.cdf(n_t, self.n_a, p) - self.cl * 0.5)
+            return abs(binom.cdf(self.n_obs, self.n_pop, p) - self.cl * 0.5)
         elif lf == 'right':  # right tailを計算　
-            return abs(1 - binom.cdf(n_t - 1, self.n_a, p) - self.cl * 0.5)
+            return abs(1 - binom.cdf(self.n_obs - 1, self.n_pop, p) - self.cl * 0.5)
         else:
             return 100000
 
@@ -45,15 +45,22 @@ class BinomCI:
 
         # 信頼上限
         upper = minimize_scalar(
-            self.diff_of_tail_area_and_cl, bounds=[0, 1], args=(self.n_x, 'left'), method='Bounded'
+            self.diff_of_tail_area_and_cl, bounds=[0, 1], args=('left'), method='Bounded'
         )
 
         # 信頼下限
         lower = minimize_scalar(
-            self.diff_of_tail_area_and_cl, bounds=[0, 1], args=(self.n_x, 'right'), method='Bounded'
+            self.diff_of_tail_area_and_cl, bounds=[0, 1], args=('right'), method='Bounded'
         )
 
-        return [lower, upper]
+        res = {
+            'interval': [lower.x, upper.x],
+            'detail': {
+                'upper': upper,
+                'lower': lower
+            }
+        }
+        return res
 
 
 class HyperCI:
@@ -61,9 +68,54 @@ class HyperCI:
     超幾何分布に関するクラス
     """
 
-    def __init__(self, n_pop, k_s, n_draw, k_s_obs, n_s, cl=0.05):
+    def __init__(self, n_pop, k_s, n_draw, k_s_obs, cl=0.05):
         self.n_pop = n_pop
         self.k_s = k_s
         self.n_draw = n_draw
         self.k_s_obs = k_s_obs
-        self.n_s = n_s
+        self.cl = cl
+
+    def diff_of_tail_area_and_cl(self, k_s_x, lf='left'):
+        """
+        :param lf: レフトテイル（left）かライトテイル（right）か
+        :return:lf='left'の場合は[0:k_s_t+1]の区間における確率分布の面積を、
+        lf='right'の場合は[k_s_t:n_pop+1]における確率分布の面積を算出し、
+        それらと信頼水準（cl: confidence level）/2の差の絶対値を算出する。
+        （最適化関数で最小化するために用いる）
+        """
+        if lf == 'left':  # left tailを計算
+            return abs(
+                hypergeom.cdf(self.k_s_obs, self.n_pop, k_s_x, self.n_draw) - self.cl * 0.5
+            )
+        elif lf == 'right':  # right tailを計算　
+            return abs(
+                1 - hypergeom.cdf(self.k_s_obs - 1, self.n_pop, k_s_x, self.n_draw) - self.cl * 0.5
+            )
+        else:
+            return 100000
+
+    def ci_sim(self, debug=False):
+        """
+        :param debug:
+        :return: sequence of objects like [lower, upper],
+        where each objects has x(calculated value) and and other info
+        """
+
+        # 信頼上限
+        upper = minimize_scalar(
+            self.diff_of_tail_area_and_cl, bounds=[0, self.n_pop], args=('left'), method='Bounded'
+        )
+
+        # 信頼下限
+        lower = minimize_scalar(
+            self.diff_of_tail_area_and_cl, bounds=[0, self.n_pop], args=('right'), method='Bounded'
+        )
+
+        res = {
+            'interval': [lower.x, upper.x],
+            'detail': {
+                'upper': upper,
+                'lower': lower
+            }
+        }
+        return res
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,9 +1,35 @@
 from unittest import TestCase
-from cisim.stats import BinomCI
+from cisim.stats import BinomCI, HyperCI
 
 
 class Testbinom(TestCase):
+    b = BinomCI(n_pop=100, n_obs=10, cl=0.05)
+
     def test_ci_sim(self):
-        b = BinomCI(n_a=100, n_x=10, cl=0.05)
-        res = b.ci_sim()
-        self.assertEqual([0.04900327615818594, 0.17622100352052053], [res[0].x, res[1].x])
+        res = self.b.ci_sim()
+        self.assertEqual(
+            [0.04900327615818594, 0.17622100352052053],
+            res['interval']
+        )
+
+
+class TestHyperCI(TestCase):
+    h = HyperCI(n_pop=100, k_s=30, n_draw=20, k_s_obs=5)
+
+    def test_hypergeom_cdf_lower(self):
+        from scipy.stats import hypergeom
+        h = HyperCI(n_pop=100, k_s=30, n_draw=20, k_s_obs=5)
+        res = hypergeom.cdf(h.k_s_obs, h.n_pop, h.k_s, h.n_draw)
+        self.assertEqual(res, 0.4009887932548518)
+
+    def test_hypergeom_cdf_upper(self):
+        from scipy.stats import hypergeom
+        h = HyperCI(n_pop=100, k_s=30, n_draw=20, k_s_obs=5)
+        res = 1 - hypergeom.cdf(h.k_s_obs - 1, h.n_pop, h.k_s, h.n_draw)
+        self.assertEqual(res, 0.7908367991741947)
+
+    def test_ci_sim(self):
+        res = self.h.ci_sim()
+        print(res['interval'])
+        self.assertEqual(1,1)
+
